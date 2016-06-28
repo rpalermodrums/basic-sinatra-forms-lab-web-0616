3148d4923dabd43241393af30a0e67575b702a3d
Ryan Palermo <stix250@gmail.com>
2016-06-28 12:10:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :'/newteam'
+  end
 
+  post '/team' do
+    @name, @coach, @pg, @sg, @pf, @sf, @c = params.values
+    # binding.pry
+    erb :'/team'
+  end
 
+  # get '/team' do
+  #
+  #   binding.pry
+  #   erb :'/team'
+  # end
 end
